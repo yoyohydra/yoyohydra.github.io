fbe9a300ca1c0a6409a4675ed8bea638f503a6d7
Sapargal13v <sapargal13v@mail.ru>
2019-04-28 17:59:19 +0600

    dd
    ss

--- js/common.js
+++ js/common.js
@@ -1,28 +1,47 @@
-$(function() {
+var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:0,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+		$('.carousel-testimony').owlCarousel({
+			autoplay: true,
+			center: true,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 1
+				},
+				1000:{
+					items: 1
+				}
+			}
+		});
 
-	
-       $("#my-menu").mmenu({
-       	extensions: [ 'theme-white' ,"pagedim-black", "fx-panels-none",  "fx-listitems-drop" ,],
-       	navbar: {
-       		title: 'ТОО Нур-Шапагат-курылыс'
-       	},
-       offCanvas: {
-       	position: 'right'
-       }
-
-       });
-
-       var api = $('#my-menu').data('mmenu');
-       api.bind('open:finish', function(){
-       	$('.hamburger').addClass('is-active');
-       }).bind('close:finish', function(){
-       	$('.hamburger').removeClass('is-active');
-});
-
-});
-
- AOS.init();
-// $(".welcome h1").animatedCss("fadeIn");
-
-
-
+	};
+	carousel();
